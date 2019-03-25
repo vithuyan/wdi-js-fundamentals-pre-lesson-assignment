c7e5d9f54c802b4210910880fb4f87043e19b364
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-03-25 00:35:59 -0400

    done

---
+++ pacman-console-log.js
@@ -0,0 +1,17 @@
+// pacman-console-log.js
+
+console.log('Pac-Man eats ghosts for lunch.');
+
+// pacman-arrays.js
+const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log('There are ${ghosts.length} ghosts.');
+console.log('Their name are: ');
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
+
+
+
+$ node pacman-arrays.js
